6cfe8c01accd33e848864e70b45bda5f17f82558
julia <julia.gru@gmail.com>
2020-10-28 23:17:28 +0100

    readme better format

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # \<searchable-dropdown>
 
 ## Local Demo with `web-dev-server`
+
 ```bash
 npm start
 ```
+
 To run a local development server that serves the basic demo located in `demo/index.html`
 
 ## Stories
@@ -13,85 +15,86 @@ To run a local development server that serves the basic demo located in `demo/in
 
 ## Bonus
 
-● Add support for handling changes to the inputs, and for setting the selected value programmatically ✅
-● Make it look nice, without polluting the css of the rest of the page ✅
-● Make it work with only a keyboard
-● Make it screen reader friendly
-● Animate changes ✅
-● Make it emit a change event when the user changes their selection ✅
+● Add support for handling changes to the inputs, and for setting the selected value programmatically ✅  
+● Make it look nice, without polluting the css of the rest of the page ✅  
+● Make it work with only a keyboard  
+● Make it screen reader friendly  
+● Animate changes ✅  
+● Make it emit a change event when the user changes their selection ✅  
 ● Add a test or two for your element to show you master that as well
 
 ## API
 
 You can set component properties by assigning values to following attributes:
 
-1. options { type: Array }
-    You must apply JSON.stringify before passing the array to the attribute.
+1. options { type: Array }  
+   You must apply JSON.stringify before passing the array to the attribute.
 
-    example:
-    ```js
-      listOfOptions = JSON.stringify(data);
-      componentElementReference.setAttribute('options', listOfOptions);
-    ```
+   example:
 
-2. selected {type: String}
-    By assigning a value to this attribute you can predefine the choice or set it up programmatically from component's parent level. 
-    🐞 - value can be set to any value, not only from the options list. Maybe it could be fixed with a propper setter on that attribute.
-
-3. disabled { type: Boolean }
-  You can disable the input adding this attribute to component's custom element. 
-
-  ```html
-   <searchable-dropdown options="" disabled > </searchable-dropdown>
+   ```js
+   listOfOptions = JSON.stringify(data);
+   componentElementReference.setAttribute('options', listOfOptions);
    ```
 
-4. placeholder { type: String },
-  This will be shown in th einput field before selection
+2. selected {type: String}  
+   By assigning a value to this attribute you can predefine the choice or set it up programmatically from component's parent level.
+   🐞 - value can be set to any value, not only from the options list. Maybe it could be fixed with a propper setter on that attribute.
 
-5. label { type: String }
-  The tiny thing telling you what this field is :)
+3. disabled { type: Boolean }  
+   You can disable the input adding this attribute to component's custom element.
+
+```html
+<searchable-dropdown options="" disabled> </searchable-dropdown>
+```
+
+4. placeholder { type: String }  
+   This will be shown in th einput field before selection
+
+5. label { type: String }  
+   The tiny thing telling you what this field is :)
 
 ## Events
 
-1. 'selected'
-    detail: value selected by user
+1. 'selected'  
+   detail: value selected by user
 
 ## Theming
 
 You can style the component by defing these custom properties:
 
 ```css
- :root {
-        --animation-time: .3s;
-        --border: solid 1px var(--primary-color);
-        --active: LightBlue; // outline and highlight color
-        --background: white;
-        --primary-color: Gainsboro; //border and button color
-        --text-color: DarkGrey;
-        --size: 10; // number of visible options
-      }
-```      
+:root {
+  --animation-time: 0.3s;
+  --border: solid 1px var(--primary-color);
+  --active: LightBlue; // outline and highlight color
+  --background: white;
+  --primary-color: Gainsboro; //border and button color
+  --text-color: DarkGrey;
+  --size: 10; // number of visible options
+}
+```
+
 ## TODO
+
 1. Add "required" attribute and feedback if the field is left empty.
 2. Keyboard support.
 3. Make it screen reader friendly.
 4. Add button to clear the input field.
-5. MAYBE: Refactor search - make component render list from filtered options array instead of turning options display off. Further research on performance and acccesibility needed. 
-6. Add "debounce" parameter and funcionality. 
+5. MAYBE: Refactor search - make component render list from filtered options array instead of turning options display off. Further research on performance and acccesibility needed.
+6. Add "debounce" parameter and funcionality.
 7. Make it more pretty.💅
-
-
-
-
 
 This webcomponent follows the [open-wc](https://github.com/open-wc/open-wc) recommendation.
 
 ## Installation
+
 ```bash
 npm i searchable-dropdown
 ```
 
 ## Usage
+
 ```html
 <script type="module">
   import 'searchable-dropdown/searchable-dropdown.js';
@@ -101,29 +104,35 @@ npm i searchable-dropdown
 ```
 
 ## Linting with ESLint, Prettier, and Types
+
 To scan the project for linting errors, run
+
 ```bash
 npm run lint
 ```
 
 You can lint with ESLint and Prettier individually as well
+
 ```bash
 npm run lint:eslint
 ```
+
 ```bash
 npm run lint:prettier
 ```
 
 To automatically fix many linting errors, run
+
 ```bash
 npm run format
 ```
 
 You can format using ESLint and Prettier individually as well
+
 ```bash
 npm run format:eslint
 ```
+
 ```bash
 npm run format:prettier
 ```
-
--- src/SearchableDropdown.js
+++ src/SearchableDropdown.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css, unsafeCSS } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 
 export class SearchableDropdown extends LitElement {
   static get styles() {
@@ -9,18 +9,18 @@ export class SearchableDropdown extends LitElement {
         justify-items: center;
         align-content: center;
         position: relative;
-        font-family: inherit;   
+        font-family: inherit;
       }
 
       .label {
         background: var(--background, white);
         color: var(--primary-color, black);
-        position:absolute;
+        position: absolute;
         top: 0;
         transform: translateY(-50%);
         left: 1em;
-        padding: 0 .8em;
-        font-size: .8em;
+        padding: 0 0.8em;
+        font-size: 0.8em;
       }
 
       .input-btn {
@@ -29,11 +29,11 @@ export class SearchableDropdown extends LitElement {
         place-items: center;
         top: 50%;
         transform: translateY(-50%) rotate(180deg);
-        right: 1em; 
+        right: 1em;
         width: 10px;
         height: 10px;
         color: var(--primary-color, grey);
-        transition: transform var(--animation-time, .3s) ease .1s;
+        transition: transform var(--animation-time, 0.3s) ease 0.1s;
       }
 
       .rotate {
@@ -45,7 +45,7 @@ export class SearchableDropdown extends LitElement {
         font-size: 1rem;
         border: var(--border, solid 1px grey);
         background: inherit;
-        color: var(--text-color, black)
+        color: var(--text-color, black);
       }
 
       input:focus {
@@ -64,14 +64,13 @@ export class SearchableDropdown extends LitElement {
         display: flex;
         flex-direction: column;
         list-style: none;
-        max-height: calc(2rem*var(--size, 5));
+        max-height: calc(2rem * var(--size, 5));
         overflow-y: scroll;
         transform: scaleY(0);
         transform-origin: 100% 0%;
-        transition: all var(--animation-time, .3s) ease .1s;
+        transition: all var(--animation-time, 0.3s) ease 0.1s;
         border: var(--border, solid 1px grey);
-        color: var(--text-color, black)
-        
+        color: var(--text-color, black);
       }
 
       input:focus ~ .options_container {
@@ -80,8 +79,8 @@ export class SearchableDropdown extends LitElement {
       }
 
       .option {
-        padding: .5em 1em;
-        transition: all var(--animation-time, .3s) ease .1s;
+        padding: 0.5em 1em;
+        transition: all var(--animation-time, 0.3s) ease 0.1s;
         cursor: pointer;
       }
 
@@ -92,12 +91,11 @@ export class SearchableDropdown extends LitElement {
 
       ul {
         margin-block-start: 0em;
-        margin-block-end: 0em;  
+        margin-block-end: 0em;
         margin-inline-start: 0px;
         margin-inline-end: 0px;
         padding-inline-start: 0px;
-        }
-
+      }
     `;
   }
 
@@ -105,9 +103,9 @@ export class SearchableDropdown extends LitElement {
     return {
       options: { type: Array },
 
-      filter: { attribute: false},
+      filter: { attribute: false },
 
-      selected: { type: String, reflect: true},
+      selected: { type: String, reflect: true },
 
       placeholder: { type: String },
 
@@ -131,28 +129,35 @@ export class SearchableDropdown extends LitElement {
 
   render() {
     return html`
-     
-     
-     <div class="input-btn" @click="${this.inputFocus}" >
+      <div class="input-btn" @click="${this.inputFocus}">
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="chevron-up"
+          class="svg-inline--fa fa-chevron-up fa-w-14"
+          role="img"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path
+            fill="currentColor"
+            d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
+          ></path>
+        </svg>
+      </div>
 
-     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-up" class="svg-inline--fa fa-chevron-up fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path></svg>
-     
-     </div>
-
-     <span class="label">${this.label}</span>
+      <span class="label">${this.label}</span>
       <input
         type="text"
-        
         placeholder="${this.placeholder || 'Select option'}"
         ?disabled="${this.disabled}"
         .value="${this.filter}"
         @input="${this.filterOptions}"
         @focus="${this.rotateBtn}"
         @blur="${this.checkValue}"
-        
-       
       />
-      
+
       <ul class="options_container hide">
         ${this.options.map(
           option =>
@@ -165,8 +170,6 @@ export class SearchableDropdown extends LitElement {
             </li>`
         )}
       </ul>
-      
-      
     `;
   }
 
@@ -177,12 +180,11 @@ export class SearchableDropdown extends LitElement {
     this.filter = selected;
     this.selected = selected;
     this.emmitValue(selected);
-   
+
     container.classList.toggle('hide');
   }
 
   emmitValue(value) {
-
     const event = new CustomEvent('selected', {
       detail: value,
       bubbles: true,
@@ -190,15 +192,13 @@ export class SearchableDropdown extends LitElement {
     });
 
     this.dispatchEvent(event);
-
   }
-
 
   filterOptions() {
     this.filter = this.shadowRoot.querySelector('input').value;
     this.options.forEach(option => {
       const index = this.options.indexOf(option);
-      const el = this.shadowRoot.querySelector(`.option${  index}`);
+      const el = this.shadowRoot.querySelector(`.option${index}`);
 
       if (option.toLowerCase().includes(this.filter.toLowerCase())) {
         el.style.display = '';
@@ -209,10 +209,13 @@ export class SearchableDropdown extends LitElement {
   checkValue() {
     const btn = this.shadowRoot.querySelector('.input-btn');
 
-    if (this.options.includes(this.filter)) 
-    {this.selected = this.filter; 
-      this.emmitValue(this.selected); }
-    else {this.filter = ''; this.selected = ''};
+    if (this.options.includes(this.filter)) {
+      this.selected = this.filter;
+      this.emmitValue(this.selected);
+    } else {
+      this.filter = '';
+      this.selected = '';
+    }
 
     btn.classList.remove('rotate');
     this.filterOptions();
@@ -220,16 +223,12 @@ export class SearchableDropdown extends LitElement {
 
   inputFocus() {
     const input = this.shadowRoot.querySelector('input');
-    this.rotateBtn;
+    this.rotateBtn();
     input.focus();
-    
-
   }
 
   rotateBtn() {
     const btn = this.shadowRoot.querySelector('.input-btn');
     btn.classList.add('rotate');
   }
-
- 
 }
